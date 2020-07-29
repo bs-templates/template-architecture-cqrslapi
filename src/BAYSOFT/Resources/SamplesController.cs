08aa1048d898e8da8370e73b8c8276301e827d9c
Ítalo Silveira <italobrian@gmail.com>
2020-07-29 15:09:31 -0300

    Update SamplesController.cs

--- src/BAYSOFT/Resources/SamplesController.cs
+++ src/BAYSOFT/Resources/SamplesController.cs
@@ -1,7 +1,10 @@
 ﻿using BAYSOFT.Abstractions.Controllers;
+using BAYSOFT.Core.Application.Default.Samples.Commands.DeleteSample;
+using BAYSOFT.Core.Application.Default.Samples.Commands.PatchSample;
+using BAYSOFT.Core.Application.Default.Samples.Commands.PostSample;
+using BAYSOFT.Core.Application.Default.Samples.Commands.PutSample;
 using BAYSOFT.Core.Application.Default.Samples.Queries.GetSampleByID;
 using BAYSOFT.Core.Application.Default.Samples.Queries.GetSamplesByFilter;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,6 +21,30 @@ namespace BAYSOFT.Resources
 
         [HttpGet("{sampleid}")]
         public async Task<ActionResult<GetSampleByIDQueryResponse>> Get(GetSampleByIDQuery request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Mediator.Send(request, cancellationToken);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PostSampleCommandResponse>> Post(PostSampleCommand request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Mediator.Send(request, cancellationToken);
+        }
+
+        [HttpPut("{sampleid}")]
+        public async Task<ActionResult<PutSampleCommandResponse>> Put(PutSampleCommand request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Mediator.Send(request, cancellationToken);
+        }
+
+        [HttpPatch("{sampleid}")]
+        public async Task<ActionResult<PatchSampleCommandResponse>> Patch(PatchSampleCommand request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Mediator.Send(request, cancellationToken);
+        }
+
+        [HttpDelete("{sampleid}")]
+        public async Task<ActionResult<DeleteSampleCommandResponse>> Delete(DeleteSampleCommand request, CancellationToken cancellationToken = default(CancellationToken))
         {
             return await Mediator.Send(request, cancellationToken);
         }
